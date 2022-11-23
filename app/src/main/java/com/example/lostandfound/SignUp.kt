5a8feed2011ce98ca954f8c652456bb42e2b4fec
Atul Kumar <atul_2101ai08@iitp.ac.in>
2022-11-23 22:55:57 +0530

    removed signup bug

--- app/src/main/java/com/example/lostandfound/SignUp.kt
+++ app/src/main/java/com/example/lostandfound/SignUp.kt
@@ -3,60 +3,56 @@ package com.example.lostandfound
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_sign_up.*
+
 
 class SignUp : AppCompatActivity() {
-    private lateinit var etEmail: EditText
-    private lateinit var etPass: EditText
+    private lateinit var etEmail : String
+    private lateinit var etPass : String
+    private lateinit var etCnfPass : String
     private lateinit var auth: FirebaseAuth
-    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        etEmail = findViewById(R.id.email)
-        etPass = findViewById(R.id.pass)
+        auth = Firebase.auth
         findViewById<Button>(R.id.signup).setOnClickListener {
-            val name = findViewById<EditText>(R.id.name).text.toString()
-            val phone = findViewById<EditText>(R.id.phone).text.toString()
-            val email = etEmail.text.toString()
-            val pass = etPass.text.toString()
+            val email = findViewById<EditText>(R.id.email).text.toString()
+            val pass = findViewById<EditText>(R.id.pass).text.toString()
             val cnfpass = findViewById<EditText>(R.id.cnfpass).text.toString()
-            val data = hashMapOf<String, Any>()
-            data["Name"] = name
-            data["Roll No"] = roll
-            data["Phone"] = phone
-            data["email"] = etEmail
-            if (email.isBlank() || pass.isBlank()) {
-                Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
-            }
-            else if (pass != cnfpass)
-                Toast.makeText(this, "Password and Confirm Password are not same", Toast.LENGTH_SHORT).show()
-            else {
-                signUpUser()
-                val cur = Firebase.auth.currentUser?.uid
-                data["uId"] = cur.toString()
-                db.collection("Users").add(data)
-                val intent = Intent(this, HomePage::class.java)
-                startActivity(intent)
-            }
+            etEmail = email
+            etPass = pass
+            etCnfPass = cnfpass
+            signUpUser()
         }
     }
     private fun signUpUser() {
-        val email = etEmail.text.toString()
-        val pass = etPass.text.toString()
-        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
-            if (it.isSuccessful) {
-                Toast.makeText(this, "Successfully Singed Up", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Sign Up Failed", Toast.LENGTH_SHORT).show()
+        val email = etEmail
+        val pass = etPass
+        val cnfpass = etCnfPass
+        Log.d("xyz", "$email $pass $cnfpass")
+        if (email.isBlank() || pass.isBlank()) {
+            Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+        }
+        else if (pass != cnfpass)
+            Toast.makeText(this, "Password and Confirm Password are not same", Toast.LENGTH_SHORT).show()
+        else {
+            auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Successfully Singed Up", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, ProfileDetails::class.java)
+                    startActivity(intent)
+                } else {
+                    Log.d("xyz", "error")
+                    Toast.makeText(this, "Sign Up Failed", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
+
 }
